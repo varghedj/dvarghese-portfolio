fef4ee98cbd33217d3e9e80b932fd8247e419e67
varghedj <dylanvarghese@gmail.com>
2023-08-19 04:10:18 -0400

    quick fix - overwrote some stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,12 +25,12 @@ export default function RootLayout({
       <body className={`{inter.className} bg-gray-50 text-gray-950 relative h-[10000px] pt-28 sm:pt-36`}>
       {/*Right Side Bubble*/}
         <div className=
-          'bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'>
+          'bg-[#fbe2e3] absolute -z-10 top-[-6rem] -right-[20rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'>
         </div>
 
       {/*Left Side Bubble*/}
         <div className=
-          'bg-[#fffcfc] absolute -z-10 top-[-1rem] left-[-35rem] h-[100%] w-[50rem] rounded-full blur-[10rem]  sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'>
+          'bg-[#fffdfd] absolute -z-10 top-[-1rem] left-[-35rem] h-[100%] w-[50rem] rounded-full blur-[10rem]  sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'>
         </div>
 
         <Header />
