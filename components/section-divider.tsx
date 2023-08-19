f18ac7f3b5bc43a67b451072a74527bbc260c4d6
varghedj <dylanvarghese@gmail.com>
2023-08-19 03:56:07 -0400

    last minute color change

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -6,11 +6,11 @@ import { delay, motion } from 'framer-motion'
 export default function SectionDivider() {
   return (
     <motion.div className=' flex items-center justify-center
-    text-gray-200 text-2xl
+    filter grayscale
+    text-4xl
     my-24 h-16 
-    rounded-full hidden 
-    sm:block'
-    //'bg-gray-200 
+    rounded-full'
+    //'bg-gray-200 hidden sm:block
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0}}
       transition={{ delay: 0.125 }}
